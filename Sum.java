08fb999c6ea0c27fee4140a0e9262a9b45bb3137
shashankreddy92 <shashu.reddy@gmail.com>
2025-06-09 19:09:06 +0530

    Second changes

--- Sum.java
+++ Sum.java
@@ -2,22 +2,28 @@ import java.util.Scanner;
 
 public class Sum {
 
-    public static void sum(int a, int b)
-    {
-      int result = a + b;
-      System.out.println("Result:"+ result);
-
+    // Method to add two numbers
+    public static int add(int a, int b) {
+        int result = a + b; ;
+        return result;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first number:");
-        int x = scanner.nextInt();
-        System.out.println("Enter second number:");
-        int y = scanner.nextInt();
-        sum(x,y);
-        
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number:"); 
+        int num2 = scanner.nextInt();
+
+        // Call the add method and store the result
+        int sum = add(num1, num2);
+
+        // Display the result
+        System.out.println("The sum is: " + sum);
     }
-    
 }
+
+    
+
+
+

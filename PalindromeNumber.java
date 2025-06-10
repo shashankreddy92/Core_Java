c9da88d34b84392f5fcb158d3da4356368fd7285
shashankreddy92 <shashu.reddy@gmail.com>
2025-06-10 18:58:45 +0530

    Commit

---
+++ PalindromeNumber.java
@@ -0,0 +1,33 @@
+ class PalindromeNumber {
+
+    public static void isPalindromeNumber(int num)
+    {
+        int rem = 0 ;
+        int sum = 0 ;
+        int temp ;
+
+        temp = num ;
+
+        while(num>0)
+        {
+            rem = num % 10;
+            sum = (sum * 10) + rem ; 
+            num = num / 10;
+        }
+
+        if(temp == sum)
+        {
+            System.out.println("Is a Palindrome Number");
+        }
+        else{
+            System.out.println("Not a Palindrome Number");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        isPalindromeNumber(121);
+    }
+
+    
+}
